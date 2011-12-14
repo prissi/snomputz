2d0f22c470490a931444dbdbbe76a646c108e831
Markus Pristovsek <markus@pristovsek.de>
2011-12-14 08:00:41 +0000

    fmax header was missing
    git-svn-id: https://subversion.assembla.com/svn/snomputz/trunk@21 648f1451-7bd4-4d4a-809e-628139b29a36

---
+++ snom-fmax.h
@@ -0,0 +1,8 @@
+/**************************************************************************************
+****	Mathematische und verwandte Routinen
+**************************************************************************************/
+
+UWORD ListOfMaxima( LPUWORD puData, LONG width, LONG height, UWORD uMaxData, LONG tolerance, XYZ_COORD **pOutputKoord );
+
+void CalcDotRadius( LPUWORD puData, const LONG w, const LONG h, const UWORD zero_level, UWORD iDotNr, XYZ_COORD *pDots, const WORD quantisation, const BOOL recenter );
+
